551429e6c5f43492c04f2a2a4efb00c18bd9dfbc
leo250742 <2507426131@qq.com>
2025-09-25 11:30:24 +0800

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-model = YOLO("/home/bns/lihao/runs/segment/4/weights/best.pt")
-result = model(source="/home/bns/lh/pywork/datasets/OSCD/images/test",conf=0.8,save=True,show_labels=False,show_conf=False,name="output")
-#result = model(source="/home/bns/lh/pywork/datasets/OSCD/images/test",save=True,save_conf=True,save_txt=True,name="output")
-print(model.info())
