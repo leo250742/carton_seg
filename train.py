62dab576aaec5285e416cfa428b8829356364c7b
leo250742 <2507426131@qq.com>
2025-09-25 16:35:06 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,13 +1,6 @@
 from ultralytics import YOLO
-model = YOLO('best.pt')
-#model.load('/home/bns/lihao/yolov11n.pt')# 加载预训练模型，如果本地没有会自动下载
+model = YOLO('/ultralytics/cfg/models/v8/SK+neck.yaml'')
+model.load('yolov8n.pt')# 加载预训练模型，如果本地没有会自动下载
 model.train(data="data.yaml", epochs=300, batch=32, imgsz=640,device="0")
-# metrics=model.val(data="/home/bns/lh/pywork/datasets/OSCD/data.yaml",save_json=True)
-# print(f"mAP:{metrics.seg.map}")
-# print(f"mAP50:{metrics.seg.map50}")
-# print(f"mAP75:{metrics.seg.map75}")
-# # from ultralytics import YOLO
-# # model = YOLO('/home/bns/lihao/ultralytics/cfg/models/v8/yolov8-pose.yaml')
-# # model.load('/home/bns/lihao/yolov8n.pt')# 加载预训练模型，如果本地没有会自动下载
-# # model.train(data="/home/bns/sevenT/xhy/sea/mmpose_dataset/yolodataset/c.yaml", epochs=300, batch=32, imgsz=640,device="0")
+
 
